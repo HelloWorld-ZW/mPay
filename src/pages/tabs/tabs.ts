e289c259be3be5c56f78c225af6b40d3289fd5ac
ZhenWang <zhenwang@ZhenWangs-Mac.local>
2017-10-29 08:23:34 -0700

    v1

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import {FriendsPage } from '../friends/friends';
+import { HistoryPage } from '../history/history';
+//import { SettingsPage } from '../settings/settings';
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = FriendsPage;
+  tab3Root = HistoryPage;
+  //tab4Root = SettingsPage;
+  constructor() {
+
+  }
+}
