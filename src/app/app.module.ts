eec266bd2824539b64fdc8c433d57a2b7357ce2f
ZhenWang <zhenwang@ZhenWangs-Mac.local>
2017-10-29 08:24:05 -0700

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FriendsPage } from '../pages/friends/friends';
+import { HistoryPage } from '../pages/history/history';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    HistoryPage,
+    FriendsPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    HistoryPage,
+    FriendsPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
