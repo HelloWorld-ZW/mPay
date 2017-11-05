0cd98af730a2fd52db82383ab426820e8038ad1f
ZhenWang <zhenwang@ZhenWangs-Mac.local>
2017-11-04 18:18:23 -0700

    sign up updated

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
 
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
+@Directive({
+  selector: '[mask]'
+})
 export class SignupPage {
+
+  fullname:string;
+  email:string;
+  phone:string;
+  password:string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -15,4 +24,8 @@ export class SignupPage {
     console.log('ionViewDidLoad SignupPage');
   }
 
+  _onNextPress(){
+    alert(this.fullname+"\n"+this.email+"\n"+this.phone+"\n"+this.password);
+    alert("TODO: Send email verification code");
+  }
 }
