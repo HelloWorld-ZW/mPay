c8bfbbecd3ed256b081ca60faf7756073e5ae34e
ZhenWang <zhenwang@ZhenWangs-Mac.local>
2017-11-07 16:11:06 -0800

    account type added

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,23 +1,25 @@
 import { Component, Directive } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { NgModel } from '@angular/forms';
 
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
-@Directive({
-  selector: '[mask]'
-})
 export class SignupPage {
+
+  nameLabel:any=" Full name";
+  nameIcon:any="md-person";
 
   fullname:string;
   email:string;
   phone:string;
   password:string;
+  type:string="personal";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+      //<ion-icon name="md-person"></ion-icon> Full name<
   }
 
   ionViewDidLoad() {
@@ -27,5 +29,17 @@ export class SignupPage {
   _onNextPress(){
     alert(this.fullname+"\n"+this.email+"\n"+this.phone+"\n"+this.password);
     alert("TODO: Send email verification code");
+  }
+
+  onTypeChange(selectedValue: any){
+    
+    if(selectedValue=="business"){
+      this.nameLabel = " Business Name";
+      this.nameIcon = "ios-stats";
+    }
+    else{
+      this.nameLabel = " Full Name";
+      this.nameIcon = "md-person";
+    }
   }
 }
