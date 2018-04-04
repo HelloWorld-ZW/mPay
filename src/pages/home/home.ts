ba3ec36dd186271e2b0580f3a07f542c12dab8df
ZW <zw@ZWs-Mac.local>
2018-04-03 19:59:49 -0700

    TODO: dec transcation response

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { LoadingController, AlertController } from 'ionic-angular';
+import { LoadingController, AlertController, ToastController } from 'ionic-angular';
 import { CommunicationProvider } from '../../providers/communication/communication';
 import { CardPage } from '../card/card';
 import { ServicesProvider } from '../../providers/services/services';
@@ -15,19 +15,22 @@ import { HelperProvider } from '../../providers/helper/helper';
 })
 export class HomePage {
 
-  username:string;
-  email:string;
-  balance:any;
-  ipAddress:string;
+  username: string;
+  email: string;
+  balance: any;
+  ipAddress: string;
+  passcode: string;
 
-  pbk:any;
-  sessKey:any;
-  sessIv:any;
+  pbk: any;
+  sessKey: any;
+  sessIv: any;
 
-  transactions: any=new Array();
-  pages:any = [CardPage];
+  cards: any = new Array();
 
-  constructor(public navCtrl: NavController, 
+  transactions: any = new Array();
+  pages: any = [CardPage];
+
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private barcodeScanner: BarcodeScanner,
     public loading: LoadingController,
@@ -35,23 +38,26 @@ export class HomePage {
     private alertCtrl: AlertController,
     public services: ServicesProvider,
     public crypto: CryptoProvider,
-    public helper: HelperProvider) {
-    
-      //console.log(navParams.data);
+    public helper: HelperProvider,
+    public toastCtrl: ToastController) {
+
+    //console.log(navParams.data);
     this.username = navParams.get("username");
     this.email = navParams.get("email");
     this.balance = navParams.get("balance");
-    
+    this.passcode = navParams.get("passcode");
+
     this.pbk = navParams.get("pbk");
     this.sessKey = navParams.get("sessKey");
     this.sessIv = navParams.get("sessIv");
     //alert(navParams.get("pbk"));
-    
+
     this.logHistory();
     //this.helper.setStorageData("loggedIn",this.email); //save logged in email locally
 
-    
-    for(var i=0; i<20; i++){
+    //alert(this.passcode);
+
+    for (var i = 0; i < 20; i++) {
       let obj = {
         "name": "Zhen",
         "Date": "01/01/2018",
@@ -59,107 +65,173 @@ export class HomePage {
       };
       this.transactions.push(obj);
     }
-    
+
     //load transaction history
+
+    this.loadCards();
 
   }
   //constructor END
 
-  _scan(){
+  _scan() {
     this.barcodeScanner.scan().then(data => {
       let loading = this.loading.create({
         content: 'Processing...'
       });
       loading.present();
-      
-      if(this.codeValidation(data.text)){
+
+      if (this.codeValidation(data.text)) {
         this.services.doPOST("mpay/transaction/getQrInfo", this.packTransCode(data.text))
-        .then((response)=>{
-          alert(response);
-          let responseJson = JSON.parse(response+"");
-          alert("response: "+responseJson.response);
-          if(responseJson.response == 1){
-            let plainStr = this.crypto.AESDecypto(responseJson.cipher, this.sessKey, this.sessIv);
-            let plainJson = JSON.parse(plainStr);
+          .then((response) => {
+            //alert(response);
+            let responseJson = JSON.parse(response + "");
+            //alert("response: " + responseJson.response);
+            if (responseJson.response == 1) {
+              let plainStr = this.crypto.AESDecypto(responseJson.cipher, this.sessKey, this.sessIv);
+              let plainJson = JSON.parse(plainStr);
 
-            let alert = this.alertCtrl.create({
-              title: 'Confirm Payment',
-              message: 'Merchant: '+plainJson.merchantName +"<br/>Amount: "+plainJson.amount,
-              buttons: [
-                {
-                  text: 'Cancel',
-                  role: 'cancel'
-                },
-                {
-                  text: 'Pay',
-                  handler: () => {
-                    alert.dismiss();
+              let alert = this.alertCtrl.create({
+                title: 'Confirm Payment',
+                subTitle: 'Merchant: ' + plainJson.merchantName + "<br/>Amount: " + plainJson.amount,
+                buttons: [
+                  {
+                    text: 'Cancel',
+                    role: 'cancel'
+                  },
+                  {
+                    text: 'Pay',
+                    handler: () => {
+                      alert.dismiss();
 
-                    //TODO: 
-                    //check balance
-                    // if enough, pop up passcode alert
-                    // else pop up select card alert
-                    this.passcodeAlert(plainJson.merchantId, data.text);
+                      //TODO: 
+                      //check balance
+                      // if enough, pop up passcode alert
+                      // else pop up select card alert
+
+                      if (this.balance < plainJson.amount) {
+                        this.selectCardAlert(plainJson.merchantId, data.text);
+                      }
+                      else {
+                        this.passcodeAlert(plainJson.merchantId, data.text, "");
+                      }
+                    }
                   }
-                }
-              ]
-            });
-            alert.present();
-          
-          }
-          else{
-            alert("response err: "+responseJson.response);
-          }
+                ]
+              });
+              alert.present();
 
-        });
-       }
-       else{
-        alert("No such code "+data.text);
-       }
+            }
+            else {
+              alert("response err: " + responseJson.response);
+            }
+
+          });
+      }
+      else {
+        alert("No such code " + data.text);
+      }
       loading.dismiss();
-     }).catch(err => {
-        alert("error: "+err);
-     });
-      
+    }).catch(err => {
+      alert("error: " + err);
+    });
+
   }
   // _scan END
 
-  codeValidation(transCode:string){
-    let prifix = transCode.substr(0,4);
-    if(prifix=='mpay')
+  codeValidation(transCode: string) {
+    let prifix = transCode.substr(0, 4);
+    if (prifix == 'mpay')
       return true;
     else
       return false;
   }
 
-  packTransCode(transCode:string){
+  packTransCode(transCode: string) {
     let dataPack = JSON.stringify({
-      "uuid": this.crypto.RSAEncypto(JSON.parse(this.helper.getDeviceInfo()).uuid+"", this.pbk),
+      "uuid": this.crypto.RSAEncypto(JSON.parse(this.helper.getDeviceInfo()).uuid + "", this.pbk),
       "code": this.crypto.RSAEncypto(transCode, this.pbk)
     });
     return dataPack;
   }
-  
-  
-  paymentConfirmation(passcode, merchant, transCode){
-    let passcodeJson = JSON.stringify(passcode);
-    let paymentInfo = JSON.stringify({
-      "sender": this.email,
-      "receiver": merchant,
-      "transCode": transCode
+
+
+  paymentConfirmation(passcode, merchant, transCode, card) {
+
+    try {
+      let passcodeJson = JSON.stringify(passcode);
+
+      let paymentInfo = {
+        "passcode": passcode,
+        "sender": this.email,
+        "receiver": merchant,
+        "transCode": transCode
+      };
+
+      if (card.toString().length > 0) {
+        paymentInfo['card'] = card;
+      }
+
+      let uuid = JSON.parse(this.helper.getDeviceInfo()).uuid;
+      let paymentInfoCipher = JSON.stringify({
+        "uuid": this.crypto.RSAEncypto(uuid + "", this.pbk),
+        "cipher": this.crypto.AESEncypto(JSON.stringify(paymentInfo), this.sessKey, this.sessIv)
+      });
+
+      this.transProcessReturns(paymentInfoCipher);
+      //alert(paymentInfoCipher);
+    } catch (e) {
+      alert(e);
+    }
+
+  }
+
+  async transProcessReturns(cipher){
+    this.services.doPOST("mpay/transaction/qrcodepayment",cipher).then((response)=>{
+      alert(response);
+    })
+  }
+
+  passcodeAlert(merchantId, transCode, number) {
+    let passcodeAlert = this.alertCtrl.create({
+      title: 'Passcode',
+      enableBackdropDismiss: false,
+      inputs: [{
+        name: 'passcode',
+        placeholder: '6 digits',
+        type: 'tel'
+      }],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Pay',
+          handler: inputData => {
+            if (inputData.passcode.toString() == this.passcode) {
+              passcodeAlert.dismiss();
+              this.paymentConfirmation(inputData.passcode.toString(), merchantId, transCode, number);
+            }
+            else {
+              let toast = this.toastCtrl.create({
+                message: 'Passcode incorrect, try again!',
+                duration: 3000,
+                position: 'top'
+              });
+              toast.present();
+            }
+
+          }
+        }]
     });
-    alert(paymentInfo);
-        
+    passcodeAlert.present({
+      keyboardClose: false
+    });
   }
 
-  passcodeAlert(merchantId, transCode){
-    let passcodeAlert= this.alertCtrl.create({
-      title: 'Passcode',
-      inputs:[{
-        name: 'passcode',
-        placeholder: '6 digits',
-        type: 'tel'
-      }],
+  selectCardAlert(merchantId, transCode) {
+    let selectCardAlert = this.alertCtrl.create({
+      title: 'Select Card',
       buttons: [
         {
           text: 'Cancel',
@@ -167,65 +239,62 @@ export class HomePage {
         },
         {
           text: 'Pay',
-          handler: inputData => {
-            passcodeAlert.dismiss();
-            this.paymentConfirmation(JSON.stringify(inputData), merchantId, transCode);
-          }
-        }]
-      });
-      passcodeAlert.present();
-  }
-
-  //TODO: 
-  selectCardAlert(){
-    let selectCardAlert= this.alertCtrl.create({
-      title: 'Passcode',
-      inputs:[{
-        name: 'passcode',
-        placeholder: '6 digits',
-        type: 'tel'
-      }],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-        },
-        {
-          text: 'Pay',
-          handler: inputData => {
+          handler: selectedCard => {
             selectCardAlert.dismiss();
-            //this.paymentConfirmation(JSON.stringify(inputData), merchantId, transCode);
+            this.passcodeAlert(merchantId, transCode, selectedCard);
           }
         }]
-      });
-      selectCardAlert.present();
+    });
+
+    selectCardAlert.setMessage("Not Enough credit, please select a card to pay");
+    (this.cards).forEach((aCard, index) => {
+      if (index == 0) {
+        selectCardAlert.addInput({
+          type: 'radio',
+          label: aCard.hidedNum,
+          value: aCard.CardNum.toString(),
+          checked: true
+        });
+      }
+      else {
+        selectCardAlert.addInput({
+          type: 'radio',
+          label: aCard.hidedNum,
+          value: aCard.CardNum.toString()
+        });
+      }
+
+    })
+
+    selectCardAlert.present();
   }
+
   //_paymentConfirmation END
 
-  openPage(index){
+  openPage(index) {
     let navPar = {
-      email:this.email,
-      pbk:this.pbk,
-      sessKey:this.sessKey,
-      sessIv:this.sessIv
+      email: this.email,
+      pbk: this.pbk,
+      sessKey: this.sessKey,
+      sessIv: this.sessIv
     };
-    this.navCtrl.push(this.pages[index],navPar);
+    this.navCtrl.push(this.pages[index], navPar);
   }
   //openPage END
 
-  async logHistory(){
-    let myIp = await this.services.getIpAddress().then(data=>{return data;});
+  async logHistory() {
+    let myIp = await this.services.getIpAddress().then(data => { return data; });
 
     let logInfoCipher = JSON.stringify({
-      "uuid":this.crypto.RSAEncypto(JSON.parse(this.helper.getDeviceInfo()).uuid+"", this.pbk),
-      "data":this.crypto.AESEncypto(JSON.stringify({
-        "email":this.email,
+      "uuid": this.crypto.RSAEncypto(JSON.parse(this.helper.getDeviceInfo()).uuid + "", this.pbk),
+      "data": this.crypto.AESEncypto(JSON.stringify({
+        "email": this.email,
         "datetime": new Date().getTime(),
-        "ip":myIp
-      }),this.sessKey, this.sessIv)
+        "ip": myIp
+      }), this.sessKey, this.sessIv)
     });
 
-    await this.services.doPOST("mpay/account/recordHistory",logInfoCipher);
+    await this.services.doPOST("mpay/account/recordHistory", logInfoCipher);
     //log history
   }
   //logHistory END
@@ -235,12 +304,37 @@ export class HomePage {
 
     setTimeout(() => {
       for (let i = 0; i < 30; i++) {
-        this.transactions.push( this.transactions.length );
+        this.transactions.push(this.transactions.length);
       }
 
       console.log('Async operation has ended');
       infiniteScroll.complete();
     }, 500);
+  }
+
+
+  loadCards() {
+    let uuid = (JSON.parse(this.helper.getDeviceInfo()).uuid) + "";
+    let cipher = JSON.stringify({
+      "uuid": this.crypto.RSAEncypto(uuid, this.pbk),
+      "email": this.crypto.RSAEncypto(this.email, this.pbk)
+    });
+    this.loadCardsRetturns(cipher)
+  }
+
+  async loadCardsRetturns(cipher) {
+
+    let response = await this.services.doPOST("mpay/registercard/loadCards", cipher).then(data => { return data; });
+
+    let responseJson = JSON.parse(response.toString());
+    let hiden = "**** **** **** ";
+    if (responseJson.response == 1) {
+      this.cards = JSON.parse(this.crypto.AESDecypto(responseJson.card, this.sessKey, this.sessIv));
+      (this.cards).forEach((aCard) => {
+        aCard.hidedNum = hiden + (aCard.CardNum.toString()).substring(12, 16);
+      });
+
+    }
   }
 
 }
