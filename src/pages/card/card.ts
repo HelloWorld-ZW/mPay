eb47b9ac9154336743911a0b78eb3d1797935ca1
ZW <zw@ZWs-Mac.local>
2018-04-19 18:51:48 -0700

    delete card

--- src/pages/card/card.ts
+++ src/pages/card/card.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ActionSheetController,LoadingController, Events } from 'ionic-angular';
+import { NavController, NavParams, ActionSheetController, LoadingController, Events, AlertController, ToastController } from 'ionic-angular';
 import { AddcardPage } from '../addcard/addcard';
 
-import {CryptoProvider} from '../../providers/crypto/crypto';
+import { CryptoProvider } from '../../providers/crypto/crypto';
 import { HelperProvider } from '../../providers/helper/helper';
 import { ServicesProvider } from '../../providers/services/services';
 
@@ -16,28 +16,30 @@ export class CardPage {
 
   cards: any = new Array();
   email: any;
-  hasCard:any;
-  
-  pbk:any;
-  sessKey:any;
-  sessIv:any;
+  hasCard: any;
 
-  constructor(public navCtrl: NavController, 
+  pbk: any;
+  sessKey: any;
+  sessIv: any;
+
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public actionSheetCtrl: ActionSheetController,
     public loadingCtrl: LoadingController,
     public crypto: CryptoProvider,
     public helper: HelperProvider,
     public services: ServicesProvider,
-    public events: Events
-    
+    public events: Events,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController
+
   ) {
 
     this.email = navParams.get("email");
     this.sessKey = navParams.get("sessKey");
     this.sessIv = navParams.get("sessIv");
     this.pbk = navParams.get("pbk");
-    
+
     this.realodCardListener();
   }
 
@@ -47,8 +49,6 @@ export class CardPage {
 
   presentActionSheet(CardNum) {
 
-    //console.log("card click = "+ CardNum);
-
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Select Option',
       buttons: [
@@ -56,19 +56,67 @@ export class CardPage {
           text: 'Delete',
           role: 'destructive',
           handler: () => {
-            console.log('Delete clicked');
-          }
-        }, {
-          text: 'View Balance',
-          handler: () => {
-            console.log('View Balance clicked');
+
+            let alert = this.alertCtrl.create({
+              title: 'Delete Card',
+              message: 'Are you sure to delete this card?',
+              buttons: [
+                {
+                  text: 'Cancel',
+                  role: 'cancel'
+                },
+                {
+                  text: 'Delete',
+                  role: 'destructive',
+                  handler: () => {
+                    let dataPlain = JSON.stringify({
+                      'email': this.email,
+                      'cardNum': CardNum
+                    });
+
+                    let uuid_cipher = this.crypto.RSAEncypto(this.helper.getDeviceUUID() + "", this.pbk);
+
+                    let dataCipher = JSON.stringify({
+                      'uuid': uuid_cipher,
+                      'data': this.crypto.AESEncypto(dataPlain, this.sessKey, this.sessIv)
+                    });
+
+                    this.services.doPOST("mpay/registercard/deleteCard", dataCipher).then((response) => {
+
+                      let responseJson = JSON.parse(response.toString());
+                      alert.dismiss();
+
+                      if (responseJson.response == 1) {
+                        let toast = this.toastCtrl.create({
+                          message: 'Card Has been removed!',
+                          duration: 3000,
+                          position: 'top'
+                        });
+
+                        toast.present();
+                        this.events.publish('reloadCards');
+                        //this.loadCards();
+
+                      } else {
+                        let toast = this.toastCtrl.create({
+                          message: responseJson.response,
+                          duration: 3000,
+                          position: 'top'
+                        });
+
+                        toast.present();
+                      }
+
+                    });
+                  }
+                }
+              ]
+            });
+            alert.present();
           }
         }, {
           text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
+          role: 'cancel'
         }
       ]
     });
@@ -76,7 +124,7 @@ export class CardPage {
   }
 
 
-  loadCards(){
+  loadCards() {
     let uuid = (JSON.parse(this.helper.getDeviceInfo()).uuid) + "";
     let cipher = JSON.stringify({
       "uuid": this.crypto.RSAEncypto(uuid, this.pbk),
@@ -98,9 +146,9 @@ export class CardPage {
     if (responseJson.response == 1) {
       this.hasCard = true;
       this.cards = JSON.parse(this.crypto.AESDecypto(responseJson.card, this.sessKey, this.sessIv));
-      (this.cards).forEach((aCard)=>{
-        aCard.hidedNum = hiden+ (aCard.CardNum.toString()).substring(12,16);
-        switch((aCard.CardNum.toString()).substring(0,1)){
+      (this.cards).forEach((aCard) => {
+        aCard.hidedNum = hiden + (aCard.CardNum.toString()).substring(12, 16);
+        switch ((aCard.CardNum.toString()).substring(0, 1)) {
           case '4':
             aCard.bgImg = "assets/imgs/visa.png";
             break;
@@ -111,17 +159,17 @@ export class CardPage {
       });
 
     }
-    else{
+    else {
       this.hasCard = false;
     }
   }
 
   addCard() {
     let navPar = {
-      email:this.email,
-      pbk:this.pbk,
-      sessKey:this.sessKey,
-      sessIv:this.sessIv
+      email: this.email,
+      pbk: this.pbk,
+      sessKey: this.sessKey,
+      sessIv: this.sessIv
     };
 
     this.navCtrl.push(AddcardPage, navPar);
